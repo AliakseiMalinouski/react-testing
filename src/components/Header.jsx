b094eb92c0fe815fcae5276fe25142d81a8aa5bd
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2024-06-11 16:30:55 +0200

    methods to available element

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
     return (
         <div>
-            Header
+            <h3>Header</h3>
         </div>
     )
 }
--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -5,5 +5,10 @@ import { render } from "@testing-library/react";
 describe('Header component tests', () => {
     test('Header render test', () => {
         const headerComponent = render(<Header/>);
+        const expectedTag = 'H3';
+        const headerText = headerComponent.getByText('Header');
+        const headerTagTextTag = headerText.tagName;
+        expect(headerText).toBeTruthy();
+        expect(headerTagTextTag).toBe(expectedTag);
     });
 });
