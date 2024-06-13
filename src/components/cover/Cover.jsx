64a1968c4a83dc323d589e62ca3839db4ccbc0b1
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2024-06-13 20:30:09 +0200

    testing list

--- src/components/cover/Cover.jsx
+++ src/components/cover/Cover.jsx
@@ -1,15 +1,29 @@
 import React from "react";
+import { useState } from "react";
 
 const Cover = ({ title, text, items }) => {
+    const [data, setData] = useState(items);
+    const handleClick = (id) => {
+        const preparedData = data.map((item) => {
+            return {
+                ...item,
+                checked: item.id === id,
+            }
+        });
+        setData(preparedData);
+    }
     return (
         <div>
             <h1>{title}</h1>
             <p>{text}</p>
-            {items.map((item) => (
-                <div key={item}>
-                    {item}
+            <div data-testid="items-test-id">
+            {data.map(({ title, checked, id }) => (
+                <div key={id} onClick={() => handleClick(id)} data-testid={`item-test-id-${id}`}>
+                    <h3>{title}</h3>
+                    <input type='checkbox' checked={checked}/>
                 </div>
             ))}
+            </div>
         </div>
     )
 }
--- src/components/cover/Cover.test.js
+++ src/components/cover/Cover.test.js
@@ -1,14 +1,59 @@
 import React from "react";
 import { Cover } from "./Cover";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 const text = 'some text';
 const title = 'some title';
-const items = ['item1', 'item2', 'item3']
+const items = [
+    {
+        id: 1,
+        title: 'item1',
+        checked: false,
+    },
+    {
+        id: 2,
+        title: 'item2',
+        checked: false,
+    },
+    {
+        id: 3,
+        title: 'item3',
+        checked: false,
+    }
+]
 
 describe('Cover tests', () => {
     test('Cover render test', () => {
        const { container } = render(<Cover text={text} title={title} items={items}/>);
        expect(container).toMatchSnapshot();
-    })
+    });
+
+    test('Should display title', () => {
+        const { queryByText } = render(<Cover text={text} title={title} items={items}/>);
+        const titleQuery = queryByText(title);
+        expect(titleQuery).toBeTruthy();
+    });
+
+    test('Should display text', () => {
+        const { queryByText } = render(<Cover text={text} title={title} items={items}/>);
+        const textQuery = queryByText(text);
+        expect(textQuery).toBeTruthy();
+    });
+
+    test('Should display items', () => {
+        const { getByTestId } = render(<Cover text={text} title={title} items={items}/>);
+        const minItemsLength = 2;
+        const testiId = 'items-test-id';
+        const itemsWrapper = getByTestId(testiId);
+        expect(itemsWrapper.children.length).toBeGreaterThan(minItemsLength);
+    });
+
+    test('onClick test', () => {
+        const { getByTestId } = render(<Cover text={text} title={title} items={items}/>);
+        const testiId = 'items-test-id';
+        const { id } = Object.values(items)[Math.floor(Math.random()) * items.length];
+        const randomItemId = `item-test-id-${id}`;
+        const randomItem = getByTestId(randomItemId);
+        fireEvent.click(randomItem);
+    });
 });
